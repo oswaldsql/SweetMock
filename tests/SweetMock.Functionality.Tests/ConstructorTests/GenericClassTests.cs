f620e92accc6ad7203bac2205a91cd59c18fee76
Lasse Sjørup <lasse.sjoerup.ext@siemensgamesa.com>
2025-04-23 19:48:23 +0200

    adding unit tests for generic tests

--- tests/SweetMock.Functionality.Tests/ConstructorTests/GenericClassTests.cs
+++ tests/SweetMock.Functionality.Tests/ConstructorTests/GenericClassTests.cs
@@ -6,18 +6,37 @@ public class GenericClassTests
     [Fact]
     public void CanCreateGenericMock()
     {
+        Action<Guid> trigger = null!;
         // Arrange & Act
-        var actual = Mock.Repo<GenericClassTests>();
+        CallLog callLog = new CallLog();
+        var actual = Mock.Repo<Guid>(config => config
+            .SomeMethod(returns: Guid.NewGuid())
+            .SomeMethod(call: Guid => Guid)
+            .SomeMethod(throws: new ArgumentException())
+            .SomeProperty(returns: Guid.NewGuid())
+            .SomeProperty(get: Guid.NewGuid, set: Set)
+            .Indexer(values: new Dictionary<string, Guid>())
+            .Indexer(get: s => Guid.NewGuid(), (s, guid) => { })
+            .SomeEvent(Guid.NewGuid())
+            .SomeEvent(out trigger).LogCallsTo(callLog)
+    );
 
-        // Assert
+        trigger(Guid.NewGuid());
+    // Assert
         Assert.NotNull(actual);
+
+        callLog.SomeMethod(args => args.input is Guid);
     }
-    
+
+    private void Set(Guid guid)
+    {
+    }
+
     public class Repo<T> where T : new()
     {
         public virtual T SomeMethod(T input) => input;
         public virtual T SomeProperty { get; set; }
         public virtual T this[string key] { get { return new T();} set{} }
-        public virtual event EventHandler<Version> NewVersionAdded;
+        public virtual event EventHandler<Guid> SomeEvent;
     }
 }
